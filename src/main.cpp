a35c78c3e984eb4ad374b1979512b5c45dc9ac1a
krish choudhary <krish.choudhary_cs.aiml24@gla.ac.in>
2026-02-20 15:33:48 +0530

    Define touch sensor pin and state variable

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,10 @@
  * Reads digital touch input from TTP223 sensor
  * and displays structured output via Serial Monitor.
  */
+// TODO 1:
+// Define touch sensor digital pin (Use pin 2)
+const int TOUCH_PIN = 2;
+
+// TODO 2:
+// Create variable to store touch state
+int touchState = 0;
